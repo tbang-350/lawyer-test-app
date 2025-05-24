bc28caa186a50d65e22504cb95edf0a94391cf65
tbway350 <tbway350@gmail.com>
2025-05-24 16:24:15 +0000

    commit 4 ?

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Legal Appointment Dashboard</title>
     <!-- Link to Material Design Icons (example using a CDN) -->
-    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet"/>
-    <link href="{{ asset('css/fullcalendar/main.css') }}" rel="stylesheet">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/calendar.js'])
 
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
 </head>
+
 <body>
 
     <div id="app">
         @yield('content')
     </div>
 
+    @stack('scripts')
+
 </body>
+
 </html>
